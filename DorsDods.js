3c0c69c64ce148436e0bb35c4f7ac1baba720c2b
Bruno Wowk <bruno.wowk@gmail.com>
2017-03-16 15:22:41 -0300

    Create DorsDods.js

---
+++ DorsDods.js
@@ -0,0 +1,38 @@
+confirmDod = function(dod) {
+  return confirm("Did you fulfill the DoD?\n"+dod);
+};
+
+dodAnalysing = "Analysing:\n\
+1. Final assets were delivered and are in the task.\n\
+2. Due dates were agreed and are present in the task.\n\
+3. The task was estimated.\n\
+4. CPs were counted during the analysis.";
+
+dodInProgress = "In Progress:\n\
+1. MTS/MTP documentation was written.\n\
+2. Unit test was done in all supported devices and browsers.\n\
+3. The task was implemented in all components.";
+
+dodCodeReview = "Code Review:\n\
+1. Code standard\n\
+2. Performance\n\
+3. Vulnerability.";
+
+dodTest = "Test:\n\
+1. Build backend and Build Drupal were generated before testing.\n\
+2. Test Cases were written and reviewed by another tester.\n\
+3. All supported devices and browser were tested.\n\
+4. The tests were ran in all components that were changed.";
+
+dodMts = "MTS:\n\
+1. Dev: Unit test was done in all supported devices and browsers.\n\
+2. Tester: All supported devices and browser were tested\n\
+3. Tester/SM: send the UAT mail.";
+
+dodUat = "UAT:\n\
+1. JnJ final approval received.";
+
+dodMtp = "MTP:\n\
+1. Dev: Unit test was done in all supported devices and browsers.\n\
+2. Tester: All supported devices and browser were tested\n\
+3. Tester/SM: send the Go Live mail.";
