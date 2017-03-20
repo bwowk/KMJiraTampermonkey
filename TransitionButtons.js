96abb2eee48a4bad4f0cbadcec06d6f306432815
Bruno Wowk <bruno.wowk@gmail.com>
2017-03-20 16:09:41 -0300

    updating to select transition button by text

--- TransitionButtons.js
+++ TransitionButtons.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vistakon Jira DoD/DoR Issue Status Transition Buttons
 // @namespace    https://jiracloud.cit.com.br
-// @version      0.1
+// @version      0.2
 // @description  Show DoD/DoR checklists
 // @author       bwowk
 // @require      https://raw.githubusercontent.com/bwowk/vistakonJiraDorDod/master/DorsDods.js
@@ -14,23 +14,30 @@
     //Analysing
     //$('#action_id_351').click(function(evt) {if(!confirmDod(dodAnalysing)) { evt.stopPropagation(); evt.preventDefault(); }});
 
-    //In Progress
-    $('#action_id_151,#action_id_41').click(function(evt) {if(!confirmDod(dodAnalysing)) { evt.stopPropagation(); evt.preventDefault(); }});
-
-    //In Review
-    $('#action_id_171').click(function(evt) {if(!confirmDod(dodInProgress)) { evt.stopPropagation(); evt.preventDefault(); }});
-
-    //In QA
-    $('#action_id_201').click(function(evt) {if(!confirmDod(dodCodeReview)) { evt.stopPropagation(); evt.preventDefault(); }});
-
-    //In MTS
-    $('#action_id_161').click(function(evt) {if(!confirmDod(dodTest)) { evt.stopPropagation(); evt.preventDefault(); }});
-
-    //In MTP
-    //$('#action_id_271').click(function(evt) {if(!confirmDod(dodMtp)) { evt.stopPropagation(); evt.preventDefault(); }});
-
-    //In UAT
-    $('#action_id_261').click(function(evt) {if(!confirmDod(dodMts)) { evt.stopPropagation(); evt.preventDefault(); }});
-
+    const PARENT_ISSUE_TYPES = ['Improvement','Content Change','Story','Legacy Bug','Incident','Service Request', 'Env and SCM'];
+    var currentIssueType = $('#type-val').text().trim();
+    if ($.inArray(currentIssueType,PARENT_ISSUE_TYPES) != -1) {
+        //In Progress
+        $('.issueaction-workflow-transition:contains("In Progress")').click(function(evt) {if(!confirmDod(dodAnalysing)) { evt.stopPropagation(); evt.preventDefault(); }});
+        $('.issueaction-workflow-transition:contains("In Progress")').append('✔');
+        //In Review
+        $('.issueaction-workflow-transition:contains("In Review")').click(function(evt) {if(!confirmDod(dodInProgress)) { evt.stopPropagation(); evt.preventDefault(); }});
+        $('.issueaction-workflow-transition:contains("In Review")').append('✔');
+        //In QA
+        $('.issueaction-workflow-transition:contains("In QA")').click(function(evt) {if(!confirmDod(dodCodeReview)) { evt.stopPropagation(); evt.preventDefault(); }});
+        $('.issueaction-workflow-transition:contains("In QA")').append('✔');
+        //In MTS
+        $('.issueaction-workflow-transition:contains("In MTS")').click(function(evt) {if(!confirmDod(dodTest)) { evt.stopPropagation(); evt.preventDefault(); }});
+        $('.issueaction-workflow-transition:contains("In MTS")').append('✔');
+        //In UAT
+        $('.issueaction-workflow-transition:contains("In UAT")').click(function(evt) {if(!confirmDod(dodMts)) { evt.stopPropagation(); evt.preventDefault(); }});
+        $('.issueaction-workflow-transition:contains("In UAT")').append('✔');
+        //In MTP
+        $('.issueaction-workflow-transition:contains("In MTP")').click(function(evt) {if(!confirmDod(dodUat)) { evt.stopPropagation(); evt.preventDefault(); }});
+        $('.issueaction-workflow-transition:contains("In MTP")').append('✔');
+        //Resolve
+        $('.issueaction-workflow-transition:contains("Resolve")').click(function(evt) {if(!confirmDod(dodMtp)) { evt.stopPropagation(); evt.preventDefault(); }});
+        $('.issueaction-workflow-transition:contains("Resolve")').append('✔');
+    }
 
 })();
