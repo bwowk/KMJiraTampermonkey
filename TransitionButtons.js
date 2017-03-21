d8322c2b32e6cc42f519b6e3207e0f05d7d98d35
Bruno Wowk <bruno.wowk@gmail.com>
2017-03-21 15:47:09 -0300

    Adding 'Bug' issue type

--- TransitionButtons.js
+++ TransitionButtons.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vistakon Jira DoD/DoR Issue Status Transition Buttons
 // @namespace    https://jiracloud.cit.com.br
-// @version      0.2
+// @version      0.3
 // @description  Show DoD/DoR checklists
 // @author       bwowk
 // @require      https://raw.githubusercontent.com/bwowk/vistakonJiraDorDod/master/DorsDods.js
@@ -14,7 +14,7 @@
     //Analysing
     //$('#action_id_351').click(function(evt) {if(!confirmDod(dodAnalysing)) { evt.stopPropagation(); evt.preventDefault(); }});
 
-    const PARENT_ISSUE_TYPES = ['Improvement','Content Change','Story','Legacy Bug','Incident','Service Request', 'Env and SCM'];
+    const PARENT_ISSUE_TYPES = ['Improvement','Content Change','Story','Legacy Bug','Bug','Incident','Service Request', 'Env and SCM'];
     var currentIssueType = $('#type-val').text().trim();
     if ($.inArray(currentIssueType,PARENT_ISSUE_TYPES) != -1) {
         //In Progress
