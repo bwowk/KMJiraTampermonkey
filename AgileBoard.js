7531184087e2dc7646eb4b5c3cdcd69da511ea5a
Bruno Marques <bmarques@ciandt.com>
2017-07-12 16:58:41 -0300

    Update AgileBoard.js

--- AgileBoard.js
+++ AgileBoard.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Vistakon Jira DoD/DoR Agile Board
 // @namespace    https://jiracloud.cit.com.br
-// @version      0.9
+// @version      1.0
 // @description  Jira Agile board improvements
 // @author       bwowk
-// @require      https://raw.githubusercontent.com/bwowk/vistakonJiraDorDod/66503f86f2293c0ac3201d7c1783c4373865ec1d/DorsDods.js
+// @require      https://raw.githubusercontent.com/bwowk/vistakonJiraDorDod/master/DorsDods.js
 // @match        https://jiracloud.cit.com.br/secure/RapidBoard.jspa?rapidView=27878*
 // @grant        none
 // ==/UserScript==
